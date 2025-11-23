327209ae508c1da6c92de0dbebf0da65a6cc8f3e
yasmeenariyaz43-hue <yasmeenariyaz43@gmail.com>
2025-11-23 21:49:49 +0530

    Update q5.js

---
+++ q5.js
@@ -0,0 +1,5 @@
+let user={name:"yasmeen",address:{ City:"kashmiar",pin:193101,geo:{lat:11.22,lng:447.22}}};
+const {address :{city,geo:{lat,lng}}}=user;
+console.log(city);
+console.log(lat);
+console.log(lng);
